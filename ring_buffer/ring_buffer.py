dbcc147ec311581cceb026434a7f2f9a62ef704d
Milo Rastgoo <msrastgoo@gmail.com>
2020-05-13 19:26:48 -0400

    initial commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
 
     def append(self, item):
-        pass
+        if len(item) > 1:
+            
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        elif len(self.storage) > 0:
+            self.storage.remove(self.storage[0])
+            self.storage.append(item)
 
     def get(self):
-        pass
+        return self.storage
